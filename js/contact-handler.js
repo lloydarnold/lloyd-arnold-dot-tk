c6be4ef1d1513a74d8df8e8e4702b5e5e0166038
Lloyd Arnold <lloarn@gmail.com>
2020-06-16 11:43:44 +0100

    Add function to verify email validity

--- js/contact-handler.js
+++ js/contact-handler.js
@@ -1,4 +1,4 @@
-function formHandler (form){
+function formHandler (form) {
   var name = form.nameIn.value;
   var email = form.emailIn.value;
   var phoneNum = form.phoneNumberIn.value;
@@ -6,4 +6,10 @@ function formHandler (form){
 
   alert ("Your name is : " + name);
 
+}
+
+function validEmail (email) {
+  var emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return emailPattern.test(email);
 }
