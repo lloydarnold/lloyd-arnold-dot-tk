1e06543f885cce832e13a0dc926d41799ae39f13
Lloyd Arnold <lloarn@gmail.com>
2020-06-16 11:33:32 +0100

    Add form handler script

---
+++ js/contact-handler.js
@@ -0,0 +1,9 @@
+function formHandler (form){
+  var name = form.nameIn.value;
+  var email = form.emailIn.value;
+  var phoneNum = form.phoneNumberIn.value;
+  var message = form.messageIn.value;
+
+  alert ("Your name is : " + name);
+
+}
