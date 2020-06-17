27baf7df80b6f1a46b49166ca925b0ea94ee1181
Lloyd Arnold <lloarn@gmail.com>
2020-06-17 13:48:37 +0100

    Add new email variables

--- php/contact-handler.php
+++ php/contact-handler.php
@@ -3,6 +3,12 @@ $message = '';
 foreach($_POST as $key => $value) {
   $message .= "$key => $value\n";
 }
-mail('larnold5189@gmail.com','test', $message);
+
+$email_from = 'form-auto@lloydarnold.tk';
+$email_subject = "New Form submission";
+$email_to = 'larnold5189@gmail.com';
+$headers .= "Reply-To: $email_from \r\n";
+
+mail('larnold5189@gmail.com',$email_subject, $message, $headers);
 header('Location: ../contact.html');
 ?>
