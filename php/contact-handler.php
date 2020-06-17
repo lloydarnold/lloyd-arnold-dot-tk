2afb5c3fedb5d070ebe8a08cf8e82db03593c7bc
Lloyd Arnold <lloarn@gmail.com>
2020-06-17 13:17:59 +0100

    Update redirect header in PHP

--- php/contact-handler.php
+++ php/contact-handler.php
@@ -4,5 +4,5 @@ foreach($_POST as $key => $value) {
   $message .= "$key => $value\n";
 }
 mail('larnold5189@gmail.com','test', $message);
-header('Location: payment.html');
+header('Location: ../contact.html');
 ?>
