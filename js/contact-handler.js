0f684390fa8980aa682ab84b99107ef0649789e8
Lloyd Arnold <lloarn@gmail.com>
2020-06-17 11:01:46 +0100

    Add colour fade function

--- js/contact-handler.js
+++ js/contact-handler.js
@@ -4,7 +4,12 @@ function formHandler (form) {
   var phoneNum = form.phoneNumberIn.value;
   var message = form.messageIn.value;
 
-  alert ("Your name is : " + name);
+  if (!validEmail(email)) {
+    highlightEmail(form);
+
+  }
+
+  /* alert ("Your name is : " + name); */
 
 }
 
@@ -12,4 +17,21 @@ function validEmail (email) {
   var emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   return emailPattern.test(email);
+}
+
+function highlightEmail (form) {
+  form.emailIn.style.border = "2px solid red";
+  fadeBack(form.emailIn, 0xBBBBBB);
+  setTimeout( function(){form.emailIn.style.border = "1px solid #BBB";}, 1500 )
+}
+
+function fadeBack (element, targetCol) {
+  // four second fade
+  var currentCol = element.style.borderColor;
+  targetCol = rgb(targetCol);
+  diff = targetCol - currentCol;
+  console.log(diff);
+  for (var i = 0; i < 40; i++) {
+
+  }
 }
