c10cd82b0de4c0bba6e0c9bf4fb5197da9d9d1a1
Lloyd Arnold <lloarn@gmail.com>
2020-06-17 11:01:59 +0100

    Add hex to RGB code

--- js/contact-handler.js
+++ js/contact-handler.js
@@ -35,3 +35,8 @@ function fadeBack (element, targetCol) {
 
   }
 }
+
+function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
+function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
+function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
+function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
