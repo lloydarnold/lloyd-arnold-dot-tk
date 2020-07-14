b5b969ede14c41c2b41294c9aa5c969b2c77d07b
Lloyd Arnold <lloarn@gmail.com>
2020-07-14 12:09:57 +0100

    Drive slideshow for chat screenshots

--- js/slideshow-driver.js
+++ js/slideshow-driver.js
@@ -1,7 +1,9 @@
 var slideIndex1 = 1;
 var slideIndex2 = 1;
+var slideIndex3 = 1;
 showSlides1(slideIndex1);
 showSlides2(slideIndex2);
+showSlides3(slideIndex3);
 
 // Next/previous controls
 function plusSlides1(n) {
@@ -53,4 +55,29 @@ function showSlides2(n) {
   }
   slides[slideIndex2-1].style.display = "block";
   dots[slideIndex2-1].className += " active";
+}
+
+function plusSlides3(n) {
+  showSlides3(slideIndex3 += n);
+}
+
+// Thumbnail image controls
+function currentSlide3(n) {
+  showSlides3(slideIndex3 = n);
+}
+
+function showSlides3(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides-3");
+  var dots = document.getElementsByClassName("dot-3");
+  if (n > slides.length) {slideIndex3 = 1}
+  if (n < 1) {slideIndex3 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex3 -1].style.display = "block";
+  dots[slideIndex3 -1].className += " active";
 }
